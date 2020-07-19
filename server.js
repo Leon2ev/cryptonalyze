e17b660636440dc2af640e64fe526d44ebccf577
Jevgeni Glazunov <glazunov@Jevgenis-MBP.lan>
2020-07-19 22:22:39 +0200

    Orginize code

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 //Dotenv.config
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const api = require('binance');
+const TelegramBot = require('node-telegram-bot-api');
 const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
 
-//Connection to Binance
-const api = require('binance');
+//Connection to Binance API
 const binanceWS = new api.BinanceWS(true);
 const binanceRest = new api.BinanceRest({
   timeout: 15000, // Optional, defaults to 15000, is the request time out in milliseconds
@@ -23,8 +24,7 @@ const binanceRest = new api.BinanceRest({
 });
 const streams = binanceWS.streams;
 
-//Connection to Telegram
-const TelegramBot = require('node-telegram-bot-api');
+//Connection to Telegram API
 const token = process.env.TELEGRAM_TOKEN;
 const chat_id = process.env.TELEGRAM_CHAT_ID;
 const bot = new TelegramBot(token, {polling: true});
@@ -43,18 +43,18 @@ let xrpPairs = [];
 let usdtPairs = [];
 const pushEachPairToArray = data => {
   console.log("Group by market")
-  for(let i = 0; i < data.length; i++){
-    if(data[i].symbol.slice(-3) === 'BTC') {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].symbol.slice(-3) === 'BTC') {
       btcPairs.push(data[i].symbol);
-    }else if(data[i].symbol.slice(-3) === 'ETH') {
+    } else if(data[i].symbol.slice(-3) === 'ETH') {
       ethPairs.push(data[i].symbol);
-    }else if(data[i].symbol.slice(-3) === 'BNB') {
+    } else if(data[i].symbol.slice(-3) === 'BNB') {
       bnbPairs.push(data[i].symbol);
-    }else if(data[i].symbol.slice(-3) === 'TRX') {
+    } else if(data[i].symbol.slice(-3) === 'TRX') {
       trxPairs.push(data[i].symbol);
-    }else if(data[i].symbol.slice(-3) === 'XRP') {
+    } else if(data[i].symbol.slice(-3) === 'XRP') {
       xrpPairs.push(data[i].symbol);
-    }else if(data[i].symbol.slice(-4) === 'USDT') {
+    } else if(data[i].symbol.slice(-4) === 'USDT') {
       usdtPairs.push(data[i].symbol);
     }
   }
@@ -62,11 +62,10 @@ const pushEachPairToArray = data => {
   setInterval(getKlines, 500000)
 }
 
-//Request klines for choosen period
-let firstRun = true
+//Request klines for choosen pair and period
 const getKlines = () => {
   console.log("Get klines")
-  for(let i = 0; i < btcPairs.length; i++){
+  for (let i = 0; i < btcPairs.length; i++) {
     binanceRest
       .klines({
         symbol: btcPairs[i],
@@ -86,7 +85,6 @@ let tradeStream
 let tickerStream
 let btcStreams = []
 let obj
-let customObjectArray = []
 const createCustomObject = data => {
 
   let weekVolumeQuote = 0;
@@ -96,8 +94,8 @@ const createCustomObject = data => {
   let coeficient = 0;
   let balance = 0;
 
-  for(let i = 0; i < data.length; i++) {
-    let symbol = data[i].symbol
+  for (let i = 0; i < data.length; i++) {
+    const symbol = data[i].symbol
     weekVolumeQuote += parseFloat(data[i].quoteAssetVolume)
     weekVolume += parseFloat(data[i].volume)
     weekTakerVolumeQuote += parseFloat(data[i].takerQuoteAssetVolume)
@@ -119,17 +117,19 @@ const createCustomObject = data => {
 }
 
 //Check if code running first time or not
+let firstRun = true
 let startStreamDone = false
+let customObjectArray = []
 const firstTimeRun = () => {
-  if(firstRun === true){
+  if (firstRun === true) {
     customObjectArray.push(obj)
     btcStreams.push(tradeStream)
     btcStreams.push(tickerStream)
-    if(startStreamDone === false){
+    if (startStreamDone === false) {
       console.log("First run")
       startStream()
     }
-  }else{
+  } else {
     updateCustomObjectArray()
   }
 }
@@ -137,7 +137,7 @@ const firstTimeRun = () => {
 //Update old array with new values
 const updateCustomObjectArray = () => {
   customObjectArray.forEach(item => {
-    if(item.symbol === obj.symbol){
+    if (item.symbol === obj.symbol) {
       item = obj
     }
   })
@@ -146,7 +146,7 @@ const updateCustomObjectArray = () => {
 //Start stream if data is ready
 const startStream = () => {
   startStreamDone = true
-  if(btcPairs.length === customObjectArray.length){
+  if (btcPairs.length === customObjectArray.length) {
     console.log("Start websocket")
     getStreams()
   } else {
@@ -172,24 +172,24 @@ const getStreams = () => {
   console.log("Get stream")
   firstRun = false
   binanceWS.onCombinedStream(btcStreams, streamEvent => {
-    for(let i = 0; i < customObjectArray.length; i++){
-      if(streamEvent.stream === customObjectArray[i].tickerStream){
+    for (let i = 0; i < customObjectArray.length; i++) {
+      if (streamEvent.stream === customObjectArray[i].tickerStream) {
         tickerData = streamEvent.data
-        if(tickerData.symbol === customObjectArray[i].symbol){
+        if (tickerData.symbol === customObjectArray[i].symbol) {
           customObjectArray[i]['dayVolume'] = parseFloat(tickerData.quoteAssetVolume)
                                                 .toFixed(2)
         }
       }
-      if(streamEvent.stream === customObjectArray[i].tradeStream){
+      if (streamEvent.stream === customObjectArray[i].tradeStream) {
         tradeData = streamEvent.data
-        if(tradeData.symbol === customObjectArray[i].symbol){
+        if (tradeData.symbol === customObjectArray[i].symbol) {
           total = tradeData.price * tradeData.quantity
           priceDifference = (tradeData.price - customObjectArray[i].weekAverage)
                             / tradeData.price * 100
         }
-        if(total > customObjectArray[i].coeficient && priceDifference <= 5){
-          if(tradeData.maker === false){
-            let buyMsgTemplate =
+        if (total > customObjectArray[i].coeficient && priceDifference <= 5){
+          if (tradeData.maker === false) {
+            const buyMsgTemplate =
             `${upGraph} #${tradeData.symbol} ${tick}BUY\n`+
             `BTC: ${total.toFixed(2)}\n`+
             `${price}Price: ${tradeData.price}\n`+
@@ -197,8 +197,8 @@ const getStreams = () => {
             `Volume(24h): ${customObjectArray[i].dayVolume} BTC\n`+
             `Balance(7d): ${customObjectArray[i].balance} BTC`
             bot.sendMessage(chat_id, buyMsgTemplate);
-          }else if(tradeData.maker === true){
-            let sellMsgTemplate =
+          } else if (tradeData.maker === true) {
+            const sellMsgTemplate =
             `${downGraph} #${tradeData.symbol} ${cross}SELL\n`+
             `BTC: ${total.toFixed(2)}\n`+
             `${price}Price: ${tradeData.price}\n`+
